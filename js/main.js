d73f95cf5bbeb795ff5c307e861489d8a1826c2a
OhKevinG <kevingutierrez@u.boisestate.edu>
2025-05-03 16:49:25 -0600

    started submit functionality

--- js/main.js
+++ js/main.js
@@ -16,3 +16,23 @@ function loaded() {
 export function sayHello() {
     return 'hello';
 }
+
+document.getElementById("send-data").onclick = function () {
+    const form = document.getElementById("submit-form");
+    const title = form.title.value;
+    const director = form.director.value;
+    const year = form.year.value;
+    const rating = form.rating.value;
+    const review = form.review.value;
+    let xhr = new XMLHttpRequest();
+    xhr.open("PUT", "https://ii03ewr3r6.execute-api.us-east-2.amazonaws.com/item");
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(JSON.stringify({
+        "title": title,
+        "director": director,
+        "year": year,
+        "rating": rating,
+        "review": review
+    }));
+    form.reset();
+}
