9f3913c14504de06071d17ead1090dd2b9c1d1c2
OhKevinG <kevingutierrez@u.boisestate.edu>
2025-05-04 15:49:12 -0600

    Data can be sent, and table can be loaded

--- js/main.js
+++ js/main.js
@@ -25,14 +25,52 @@ document.getElementById("send-data").onclick = function () {
     const rating = form.rating.value;
     const review = form.review.value;
     let xhr = new XMLHttpRequest();
-    xhr.open("PUT", "https://ii03ewr3r6.execute-api.us-east-2.amazonaws.com/item");
+    xhr.open("PUT", "https://ii03ewr3r6.execute-api.us-east-2.amazonaws.com/items");
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send(JSON.stringify({
-        "title": title,
+        "id": title,
         "director": director,
         "year": year,
         "rating": rating,
         "review": review
     }));
     form.reset();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log("Status:", xhr.status);
+            console.log("Response:", xhr.responseText);
+        }
+    };
+    
+}
+
+document.getElementById("load-data").onclick = function () {
+    let lambda = document.getElementById("lambda-info");
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener("load", function () {
+        const data = xhr.response;
+        lambda.innerHTML =
+            `<thead>
+                <tr>
+                    <th>TITLE</th>
+                    <th>DIRECTOR</th>
+                    <th>YEAR</th>
+                    <th>RATING</th>
+                    <th>REVIEW</th>
+                </tr>
+            </thead>
+            <tbody>
+            ${data.map((item, index) =>
+                `<tr>
+                    <th scope="row">${item.id}</th>
+                    <td>${item.director}</td>
+                    <td>${item.year}</td>
+                    <td>${item.rating}</td>
+                    <td>${item.review}</td>
+                </tr>`).join('')}
+            </tbody>`;    
+    });
+    xhr.open("GET", "https://ii03ewr3r6.execute-api.us-east-2.amazonaws.com/items")
+    xhr.send();
 }
